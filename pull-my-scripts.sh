99ed6c6fe15aa8b1ffdfbe21dc06268d2b513ee1
zedeecs <61963989+zedeecs@users.noreply.github.com>
2021-12-06 17:25:11 +0800

    Update pull-my-scripts.sh

--- pull-my-scripts.sh
+++ pull-my-scripts.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-# pull 脚本，从github
-location=tmp
+# pull scripts form Github in MY-SHELL-SCRIPTS repository
 
-test -w /$location/v2ray-restart.sh ; echo $?
-# 检查文件是否存在并且可写入, 错误会返回1
+pull_script() {
+    wget -P $tmpdir "https://github.com/zedeecs/my-shell-scripts/raw/main/$file"
+    # 下载到指定目录
+}
 
-wget -P /$location https://github.com/zedeecs/my-shell-scripts/raw/main/v2ray-restart.sh
-# 下载到指定目录
+mv_script() {
+    echo -e "mv $tmpdir'/'$file /tmp"
+}
+
+creat_tmpdir() {
+    tmpdir=$(mktemp -d --tmpdir sync_scripts.XXXXXX)
+}
+
+main() {
+    for file in $(ls -a $(pwd)); do
+        if [ ! -z $(echo $file | grep -e ^[[:alnum:]].*[.]sh\$) ]; then # grep 语句里的 '[.]' 匹配 '.' , 用 '\.' 不成功
+            echo $file
+            pull_script $file
+            move_script
+        fi
+    done
+}
+
+# wget -P /$location https://github.com/zedeecs/my-shell-scripts/raw/main/v2ray-restart.sh
+# # 下载到指定目录
+
+creat_tmpdir
+echo tmpdir = $tmpdir
+main $@
