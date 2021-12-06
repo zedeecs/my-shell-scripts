2564ef94653405e1edd74be91871428d61d4342b
zedeecs <61963989+zedeecs@users.noreply.github.com>
2021-12-06 17:27:06 +0800

    Update pull-my-scripts.sh

--- pull-my-scripts.sh
+++ pull-my-scripts.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 # pull scripts form Github in MY-SHELL-SCRIPTS repository
 
+echo_clr() {
+    while [[ "$#" -gt '0' ]]; do
+        case $(echo $1 | tr 'A-Z' 'a-z') in
+        '-black')
+            echo -e "\e[30m$2\e[0m"
+            break
+            ;;
+        '-red')
+            echo -e "\e[31m$2\e[0m"
+            break
+            ;;
+        '-green')
+            echo -e "\e[32m$2\e[0m"
+            break
+            ;;
+        '-yellow')
+            echo -e "\e[33m$2\e[0m"
+            break
+            ;;
+        '-blue')
+            echo -e "\e[34m$2\e[0m"
+            break
+            ;;
+        '-white')
+            echo -e "\e[37m$2\e[0m"
+            break
+            ;;
+        *)
+            echo "usag: echo_clr "-color "string"""
+            break
+            ;;
+        esac
+    done
+}
+
 pull_script() {
     wget -P $tmpdir "https://github.com/zedeecs/my-shell-scripts/raw/main/$file"
     # 下载到指定目录
